392114c8ee2b4a6c768c8bb8185ece430cf002a6
joel <joelthobias447@gmail.com>
2020-11-03 22:01:52 +0530

    renoved node modules

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,34 @@
 const mongoclient = require('mongodb').MongoClient
 
-
-const state = {
-        db: null
+const state={
+        db:null
 }
 
+
 module.exports.connect = (done) => {
-        const url = 'mongodb://localhost:27017'
-        const dbname = 'shopping'
+        //const url = ''
+        
+const url = 'mongodb+srv://joel:Qwertyuiop@1@cluster0.chsu5.mongodb.net/admin'
+const dbname='shopping'
 
-
-        mongoclient.connect(url, (err, data) => {
-                if (err) return done(err)
-                state.db = data.db(dbname)
+        mongoclient.connect(url,(err, data) => {
+                 if (err) return(err)
+                state.db=data.db(dbname)
                 done()
         })
 
 }
-
-module.exports.get = () => {
+module.exports.get=()=>{
         return state.db
 }
+
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Joel:Qwertyuiop%401%0A@cluster0.ef50f.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
